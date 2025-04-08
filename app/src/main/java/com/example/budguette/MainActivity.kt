b3ea85c8b80742aa91ebd4e7767a76f5484aaa54
Hunter Smith <huntertsmith4778@gmail.com>
2025-04-08 02:44:34 -0400

    Added initial bottom nav bar

--- app/src/main/java/com/example/budguette/MainActivity.kt
+++ app/src/main/java/com/example/budguette/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.budguette
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> loadFragment(HomeFragment())
+                R.id.nav_profile -> loadFragment(ProfileFragment())
+                R.id.nav_expenses -> loadFragment(ExpensesFragment())
+                R.id.nav_subscriptions -> loadFragment(SubscriptionsFragment())
+                R.id.nav_forums -> loadFragment(ForumsFragment())
+            }
+            true
         }
+
+        // Load the default fragment
+        if (savedInstanceState == null) {
+            bottomNav.selectedItemId = R.id.nav_home
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
