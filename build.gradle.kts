9d8ef6c12036f73abeb4d364f741b0303b1b5b71
Hunter Smith <huntertsmith4778@gmail.com>
2025-04-10 15:31:10 -0400

    Initialized firebase connection for implementation of user database.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,19 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+buildscript {
+    val kotlin_version = "2.1.0"  // Update Kotlin version to match dependencies
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.google.gms:google-services:4.4.2")  // Ensure Google services version is correct
+    }
 }
